6e8d771ed1372d82a163986aa90972be4d36fbf7
parkkunghyun <79096673+parkkunghyun@users.noreply.github.com>
2022-02-25 10:39:24 +0900

    Create Ballot.sol

---
+++ Ballot.sol
@@ -0,0 +1,167 @@
+pragma solidity >= 0.4.24 <= 0.5.6;
+
+/** 
+ * @title Ballot
+ * @dev Implements voting process along with vote delegation
+ */
+contract Ballot {
+   
+    struct Voter {
+        uint weight; // weight is accumulated by delegation
+        bool voted;  // if true, that person already voted
+        address delegate; // person delegated to
+        uint vote;   // index of the voted proposal
+    }
+
+    struct Proposal {
+        // If you can limit the length to a certain number of bytes, 
+        // always use one of bytes1 to bytes32 because they are much cheaper
+        bytes32 name;   // short name (up to 32 bytes)
+        uint voteCount; // number of accumulated votes
+    }
+
+    address public chairperson;
+
+    mapping(address => Voter) public voters;
+
+    Proposal[] public proposals;
+
+    address payable VendingMachineAddress;
+    // 이거는 투표함이라고 생각하면 편함
+
+    // balance of ether in the smart contract
+    uint public DAObalance;
+
+      uint public voteEndTime;// 이거는 끝날때 시간인데 블록만들어지는것과 연관있음
+
+    // 애는 간단하게 끝나는거 알려주는 애 기본적인 디폴트 값은 false 로 맞춰저 있다.
+      bool ended;
+
+    //유권자의 제안 결정 즉 몇개나 투표되었는지인가보네
+        uint decision;
+
+    // allow withdrawals
+        mapping(address=>uint) balances;
+
+    /** 
+     * @dev Create a new ballot to choose one of 'proposalNames'.
+     * @param proposalNames names of proposals
+     */
+    constructor(bytes32 [] memory proposalNames,address payable _VendingMachineAddress, uint _voteTime) public {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+        VendingMachineAddress = _VendingMachineAddress;
+        voteEndTime = block.timestamp + _voteTime;
+
+
+        for (uint i = 0; i < proposalNames.length; i++) {
+            // 'Proposal({...})' creates a temporary
+            // Proposal object and 'proposals.push(...)'
+            // appends it to the end of 'proposals'.
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
+        }
+    }
+    
+    /** 
+     * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
+     * @param voter address of voter
+     */
+    function giveRightToVote(address voter) public {
+        require(
+            msg.sender == chairperson,
+            "Only chairperson can give right to vote."
+        );
+        require(
+            !voters[voter].voted,
+            "The voter already voted."
+        );
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;
+    }
+
+    /**
+     * @dev Delegate your vote to the voter 'to'.
+     * @param to address to which vote is delegated
+     */
+    function delegate(address to) public {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "You already voted.");
+        require(to != msg.sender, "Self-delegation is disallowed.");
+
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+
+            // We found a loop in the delegation, not allowed.
+            require(to != msg.sender, "Found loop in delegation.");
+        }
+        sender.voted = true;
+        sender.delegate = to;
+        Voter storage delegate_ = voters[to];
+        if (delegate_.voted) {
+            // If the delegate already voted,
+            // directly add to the number of votes
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            // If the delegate did not vote yet,
+            // add to her weight.
+            delegate_.weight += sender.weight;
+        }
+    }
+
+    /**
+     * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
+     * @param proposal index of proposal in the proposals array
+     */
+    function vote(uint proposal) public {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = proposal;
+
+        // If 'proposal' is out of the range of the array,
+        // this will throw automatically and revert all
+        // changes.
+        proposals[proposal].voteCount += sender.weight;
+    }
+
+    /** 
+     * @dev Computes the winning proposal taking all previous votes into account.
+     * @return winningProposal_ index of winning proposal in the proposals array
+     */
+    function countVote() public
+            returns (uint winningProposal_)
+            
+    {
+        require(
+            block.timestamp > voteEndTime,
+            "Vote not yet ended.");
+        
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+                
+                decision = winningProposal_;
+                ended = true;
+            }
+        }
+    }
+
+    
+   // Individuals can only withdraw what they deposited.
+   // After EndVote function is run and if proposal "buy_cupcakes" won,
+   // users will not be able to withdraw ether
+    function withdraw(uint amount) public{
+        if(balances[msg.sender]>=amount){
+        balances[msg.sender]-=amount;
+        msg.sender.transfer(amount);
+        DAObalance = address(this).balance;
+        }
+    }
+   
+}
